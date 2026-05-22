f7cad15acddc1a0746fae0f3115f10217d09491d
Your Name <you@example.com>
2026-05-23 01:09:12 +0200

    generated home page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { HomePage } from '@core/pages';
+
+export const routes: Routes = [{ path: '', component: HomePage }];
